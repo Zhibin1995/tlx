8881069eebd959a5f6e9af931a62bed1bc5dcff7
zhibin <825457712@qq.com>
2019-11-06 23:33:54 +0800

    fix

--- api/modules/v1/controllers/UserController.php
+++ api/modules/v1/controllers/UserController.php
@@ -11,9 +11,12 @@ namespace api\modules\v1\controllers;
 
 
 use api\controllers\OnAuthController;
+use common\enums\CacheKeyEnum;
+use common\helpers\ResultDataHelper;
 use common\models\app\Address;
 use common\models\app\Member;
 use common\models\common\Provinces;
+use Yii;
 
 class UserController  extends OnAuthController
 {
@@ -22,7 +25,23 @@ class UserController  extends OnAuthController
     public function actionPhone(){
         $post = $this->getPost();
         $member_id = $post['member_id'];
-        $userphone = $post['userphone'];
+        $iv = $post['iv'];
+        $encryptedData = $post['encryptedData'];
+        $auth_key = $post['auth_key'];
+        $auth = Yii::$app->cache->get(CacheKeyEnum::API_MINI_PROGRAM_LOGIN . $$auth_key);
+        if(!$auth){
+            return ResultDataHelper::api(403, '登陆已失效');
+        }
+        $aesKey = $auth['session_key'];
+
+        $aesIV=base64_decode($iv);
+
+        $aesCipher=base64_decode($encryptedData);
+
+        $result=openssl_decrypt( $aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
+
+        $dataObj=json_decode( $result );
+        $userphone = $dataObj->purePhoneNumber;
         $member = Member::findOne($member_id);
         $member->userphone = $userphone;
         return $member->save();
